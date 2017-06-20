09639b6689d76918827ea67433372c7b97769f28
tyronewyatt <tyronewyatt@users.noreply.github.com>
2017-06-20 16:21:29 +1000

    Import new AD users from CSV

---
+++ New-ADUser-Import-CSV.ps1
@@ -0,0 +1,15 @@
+Import-Module ActiveDirectory
+
+$Users = Import-Csv -Delimiter "," -Path ".\New-ADUser-Import.csv"            
+foreach ($User in $Users)            
+{            
+    $DisplayName = $User.'FirstName' + " " + $User.'LastName'
+    $UserFirstName = $User.'FirstName'
+    $UserLastName = $User.'LastName'
+    $OrganisationalUnit = $User.'OrganisationalUnit'
+    $AccountName = $User.'AccountName'
+    $PrincipalName = $User.'AccountName' + "@" + $User.'PrincipalName'
+    $Description = $User.'Description'
+    $Password = $User.'Password'
+    New-ADUser -Name "$AccountName" -DisplayName "$DisplayName" -SamAccountName $AccountName -UserPrincipalName $PrincipalName -GivenName "$UserFirstName" -Surname "$UserLastName" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OrganisationalUnit" -ChangePasswordAtLogon $true –PasswordNeverExpires $false -AllowReversiblePasswordEncryption $false -Verbose
+}
