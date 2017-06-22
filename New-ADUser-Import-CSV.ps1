992edc3db445261164de44b19a5b58bc381a4ef7
tyronewyatt <tyronewyatt@users.noreply.github.com>
2017-06-23 08:19:28 +1000

    Add groupmember

--- New-ADUser-Import-CSV.ps1
+++ New-ADUser-Import-CSV.ps1
@@ -11,5 +11,7 @@ foreach ($User in $Users)
     $PrincipalName = $User.'AccountName' + "@" + $User.'DomainName'
     $Description = $User.'Description'
     $Password = $User.'Password'
+    $GroupMember = $User.'$GroupMember'
     New-ADUser -Name "$AccountName" -DisplayName "$DisplayName" -SamAccountName $AccountName -UserPrincipalName $PrincipalName -GivenName "$UserFirstName" -Surname "$UserLastName" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OrganisationalUnit" -ChangePasswordAtLogon $true –PasswordNeverExpires $false -AllowReversiblePasswordEncryption $false
+    Add-ADGroupMember -Identity "$AccountName" "$GroupMember"
 }
