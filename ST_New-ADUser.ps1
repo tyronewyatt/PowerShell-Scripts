7d3f0479948d512b18aa11aced76890118f873be
Tyrone Wyatt <tw_tec@tallangatta-sc.vic.edu.au>
2017-06-24 22:39:26 +1000

    file path change

--- ST_New-ADUser.ps1
+++ ST_New-ADUser.ps1
@@ -6,7 +6,7 @@ $DomainName='tallangatta-sc.vic.edu.au'
 $Description='Student'
 $ExistingStudents = Get-ADUser -SearchBase $OrganisationalUnitBase -Filter * -Properties samAccountName
 
-$Students = Import-Csv -Delimiter "," -Path ".\ST_$SchoolNumber.csv" | Where-Object {$_.STATUS -match 'ACTV|LVNG'}
+$Students = Import-Csv -Delimiter "," -Path "C:\ST_$SchoolNumber.csv" | Where-Object {$_.STATUS -match 'ACTV|LVNG'}
 ForEach ($Student In $Students)
 {
     $AccountName = $Student.'STKEY'
@@ -14,15 +14,14 @@ ForEach ($Student In $Students)
 	$FirstName = $Student.'FIRST_NAME'
 	$SecondName = $Student.'SECOND_NAME'
 	$PreferredName = $Student.'PREF_NAME'
-	if ($Student.'SECOND_NAME'.length -eq '0')
+	If ($Student.'SECOND_NAME'.length -eq '0')
 		{$SecondNameInitial = $null}
 		else
-		{if ($Student.'SECOND_NAME'.length -eq '1')
+		{If ($Student.'SECOND_NAME'.length -eq '1')
 			{$SecondNameInitial = $Student.'SECOND_NAME'}
 			else
-			{$SecondNameInitial = $Student.'SECOND_NAME'.Substring(0,1)
-			}
-	}
+			{$SecondNameInitial = $Student.'SECOND_NAME'.Substring(0,1)}
+		}
 	$DisplayName = $Student.'FIRST_NAME' + " " + $Student.'SURNAME'
     $OrganisationalUnit = "OU=" + $Student.'TAG' + "," + $OrganisationalUnitBase
 	$GroupMember = $Description + "s " + $Student.'TAG'
@@ -30,8 +29,8 @@ ForEach ($Student In $Students)
     $PrincipalName = $AccountName + "@" + $DomainName
 	if (($ExistingStudents | Where-Object {$_.sAMAccountName -eq $AccountName}) -eq $null)
         {
-		#New-ADUser -Name "$AccountName" -DisplayName "$DisplayName" -SamAccountName $AccountName -UserPrincipalName $PrincipalName -GivenName "$StudentFirstName" -Surname "$StudentLastName" -Initials "SecondNameInitial" -Description "$Description - Start Date $StartDate" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OrganisationalUnit" -ChangePasswordAtLogon $true –PasswordNeverExpires $false -AllowReversiblePasswordEncryption $false
+		#New-ADUser -Name "$AccountName" -DisplayName "$DisplayName" -SamAccountName $AccountName -UserPrincipalName $PrincipalName -GivenName "$StudentFirstName" -Surname "$StudentLastName" -Initials "$SecondNameInitial" -Description "$Description - Start Date $StartDate" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OrganisationalUnit" -ChangePasswordAtLogon $true –PasswordNeverExpires $false -AllowReversiblePasswordEncryption $false
 		#ADGroupMember -Identity "$GroupMember" -Member "$AccountName"
-		Write-Host $AccountName $LastName $FirstName $SecondNameInitial $DisplayName $OrganisationalUnit $PrincipalName $GroupMember "$Description - Start Date $StartDate"
+		Write-Host $AccountName $LastName $FirstName $SecondNameInitial $DisplayName $OrganisationalUnit $PrincipalName $GroupMember "$Description - Start date $StartDate" $SecondNameInitial
 		}
 }
