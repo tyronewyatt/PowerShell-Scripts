058b3fbe245c863a32c9c8021442d3549bfea856
Tyrone Wyatt <tw_tec@tallangatta-sc.vic.edu.au>
2017-06-25 09:43:30 +1000

    Password vars needed fixing

--- ADUser-Expiry-Temporary-Password.ps1
+++ ADUser-Expiry-Temporary-Password.ps1
@@ -1,19 +1,19 @@
 Import-Module ActiveDirectory
 
-$OrganisationalUnit='OU=Domain Users,DC=tallangatta-sc,DC=vic,DC=edu,DC=au'
-$InitialPasswordAge='30'
-$TemporaryPasswordAge='10'
-$Users=Get-ADUser -SearchBase $OrganisationalUnit -Filter {Enabled -eq $True} -Properties samAccountName,pwdLastSet,lastLogonTimestamp,whenChanged,Description
+$InitialPasswordAge = '30'
+$TemporaryPasswordAge = '10'
+$OrganisationalUnit = 'OU=Domain Users,DC=tallangatta-sc,DC=vic,DC=edu,DC=au'
+$Users = Get-ADUser -SearchBase $OrganisationalUnit -Filter {Enabled -eq $True} -Properties samAccountName,pwdLastSet,lastLogonTimestamp,whenChanged,Description
 
 ForEach ($User In $Users)
 {
-    $samAccountName = $User.'samAccountName'
+	$InitialPasswordAge='30'
+	$TemporaryPasswordAge='10'
+	$samAccountName = $User.'samAccountName'
 	$pwdLastSet = $User.'pwdLastSet'
 	$lastLogonTimestamp = $User.'lastLogonTimestamp'
 	$whenChanged = $User.'whenChanged'
 	$Description = $User.'Description'
-	$InitialPasswordAge = $_.InitialPasswordAge
-	$TemporaryPasswordAge = $_.TemporaryPasswordAge
 	$DateString = (Get-Date).ToString()
 	
 If 	($Users | Where-Object `
@@ -24,14 +24,13 @@ If 	($Users | Where-Object `
 		}
 	)
 	{
-	#Write-Host $samAccountName $Description - Initial password expired $DateString
 	Disable-ADAccount -Identity $samAccountName
 	if($?)
 		{
 		Set-ADUser -Identity $samAccountName -Description "$Description - Initial password expired $DateString"
+		Write-Host $samAccountName 'Initial password expired.'
 		}
 	}
-
 If 	($Users | Where-Object `
 		{ `
 		$pwdLastSet -eq $False -And `
@@ -40,11 +39,11 @@ If 	($Users | Where-Object `
 		}
 	)
 	{
-	#Write-Host $samAccountName $Description - Temporary password expired $DateString
 	Disable-ADAccount -Identity $samAccountName
 	if($?)
 		{
 		Set-ADUser -Identity $samAccountName -Description "$Description - Temporary password expired $DateString"
+		Write-Host $samAccountName 'Temporary password expired.'
 		}
 	}
 }
