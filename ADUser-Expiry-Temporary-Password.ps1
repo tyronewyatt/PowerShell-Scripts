d861c8dfa5c0cd2613628999be9e9750cd9ac0af
tyronewyatt <tyronewyatt@users.noreply.github.com>
2017-06-24 13:06:33 +1000

    Rename AccountSearchBase to OrganisationalUnit

--- ADUser-Expiry-Temporary-Password.ps1
+++ ADUser-Expiry-Temporary-Password.ps1
@@ -5,7 +5,7 @@ Import-Module ActiveDirectory
 $ExpiredPasswordTask="Write-Host" #Testing
 
 #Limit AD SearchBase to OU
-$AccountSearchBase="OU=Domain Users,DC=domain,DC=local"
+$OrganisationalUnit="OU=Domain Users,DC=domain,DC=local"
 
 #Number of days a new accounts password lasts before it expires
 $InitialPasswordAge='30'
@@ -15,6 +15,6 @@ $TemporaryPasswordAge='10'
 
 # Search AD and perform task
 Write-Host "Disable accounts after $InitialPasswordAge days if initial password not changed:"
-Get-ADUser -SearchBase $AccountSearchBase -Filter {Enabled -eq $True} -Properties Name,pwdLastSet,lastLogonTimestamp,whenChanged | Where-Object {($_.pwdLastSet -eq $False) -AND ($_.lastLogonTimestamp -eq $null) -AND ($_.whenChanged -lt (Get-Date).AddDays(-($InitialPasswordAge)))} | & $ExpiredPasswordTask -Verbose
+Get-ADUser -SearchBase $OrganisationalUnit -Filter {Enabled -eq $True} -Properties Name,pwdLastSet,lastLogonTimestamp,whenChanged | Where-Object {($_.pwdLastSet -eq $False) -AND ($_.lastLogonTimestamp -eq $null) -AND ($_.whenChanged -lt (Get-Date).AddDays(-($InitialPasswordAge)))} | & $ExpiredPasswordTask -Verbose
 Write-Host "Disable accounts after $TemporaryPasswordAge days if temporary password not changed:"
-Get-ADUser -SearchBase $AccountSearchBase -Filter {Enabled -eq $True} -Properties Name,pwdLastSet,lastLogonTimestamp,whenChanged | Where-Object {($_.pwdLastSet -eq $False) -AND ($_.lastLogonTimestamp -ne $null) -AND ($_.whenChanged -lt (Get-Date).AddDays(-($TemporaryPasswordAge)))} | & $ExpiredPasswordTask -Verbose
+Get-ADUser -SearchBase $OrganisationalUnit -Filter {Enabled -eq $True} -Properties Name,pwdLastSet,lastLogonTimestamp,whenChanged | Where-Object {($_.pwdLastSet -eq $False) -AND ($_.lastLogonTimestamp -ne $null) -AND ($_.whenChanged -lt (Get-Date).AddDays(-($TemporaryPasswordAge)))} | & $ExpiredPasswordTask -Verbose
