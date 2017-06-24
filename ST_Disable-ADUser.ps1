a2ebb17ff4ac138ef284b162ddc6a9166f041f32
Tyrone Wyatt <tw_tec@tallangatta-sc.vic.edu.au>
2017-06-24 22:39:52 +1000

    file path change

--- ST_Disable-ADUser.ps1
+++ ST_Disable-ADUser.ps1
@@ -4,7 +4,7 @@ $SchoolNumber='8370'
 $StudentsOrganisationalUnit='OU=Students,OU=Domain Users,DC=tallangatta-sc,DC=vic,DC=edu,DC=au'
 $ExistingStudents = Get-ADUser -SearchBase $StudentsOrganisationalUnit -Filter {Enabled -eq $True} -Properties samAccountName,Description
 
-$Students = Import-Csv -Delimiter "," -Path ".\ST_$SchoolNumber.csv" | Where-Object {$_.STATUS -match 'LEFT|DEL'}
+$Students = Import-Csv -Delimiter "," -Path "C:\ST_$SchoolNumber.csv" | Where-Object {$_.STATUS -match 'LEFT|DEL'}
 ForEach ($Student In $Students)
 {
     $AccountName = $Student.'STKEY'
