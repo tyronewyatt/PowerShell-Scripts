f93b62f39050c3598e6d2068f09aba6982743c96
tyronewyatt <tyronewyatt@users.noreply.github.com>
2017-06-24 13:03:44 +1000

    Rename SearchBase to OrganisationalUnit

--- ADUser-Clear-PasswordDoesNotExpire.ps1
+++ ADUser-Clear-PasswordDoesNotExpire.ps1
@@ -1,8 +1,8 @@
 Import-Module ActiveDirectory
 
 #Limit AD SearchBase to OU
-$AccountSearchBase="OU=Domain Users,DC=domain,DC=local"
+$OrganisationalUnit="OU=Domain Users,DC=domain,DC=local"
 
 # Search AD and perform task
 Write-Host 'Clear password never expire flag from the following accounts:'
-Get-ADUser -SearchBase $AccountSearchBase -Filter {Enabled -eq $True -AND PasswordNeverExpires -eq $True} -Properties Name,samAccountName | ForEach {Set-ADUser -Verbose -Identity $_.samAccountName -PasswordNeverExpires $False}
+Get-ADUser -SearchBase $OrganisationalUnit -Filter {Enabled -eq $True -And PasswordNeverExpires -eq $True} -Properties samAccountName | ForEach {Set-ADUser -Verbose -Identity $_.samAccountName -PasswordNeverExpires $False}
