958d69ed2fd5101ba1eb2a9a0ea73047c5b83fc9
Tyrone Wyatt <tw_tec@tallangatta-sc.vic.edu.au>
2017-06-25 18:19:09 +1000

    complex password fuction

--- ST_New-ADUser.ps1
+++ ST_New-ADUser.ps1
@@ -4,6 +4,19 @@ $SchoolNumber = '8370'
 $OrganisationalUnitBase = 'OU=Students,OU=Domain Users,DC=tallangatta-sc,DC=vic,DC=edu,DC=au'
 $DomainName = 'tallangatta-sc.vic.edu.au'
 $Description = 'Student'
+$PasswordLength = '7'
+
+Function ComplexPassword
+	{
+	$UppercaseCharacters = (-join ('abcdefghijkmnopqrstuvwxyz'.ToCharArray() | Get-Random -Count 4))
+	$LowercaseCharacters = (-join ('ABCEFGHJKLMNPQRSTUVWXYZ'.ToCharArray() | Get-Random -Count 4))
+	$Base10Digits = (-join ('1234567890'.ToCharArray() | Get-Random -Count 2))
+	$NonalphanumericCharacters = (-join ("~!@#$%^&*_-+=`|\(){}[]:;<>,.?/".ToCharArray() | Get-Random -Count 2))
+	$ComplexityRequirements = $UppercaseCharacters + $LowercaseCharacters + $Base10Digits + $NonalphanumericCharacters
+	$ComplexPassword = -join ($ComplexityRequirements.ToCharArray() | Get-Random -Count $PasswordLength)
+	Write-Output $ComplexPassword
+	}
+
 $ExistingStudents = Get-ADUser -SearchBase $OrganisationalUnitBase -Filter * -Properties samAccountName
 
 $Students = Import-Csv -Delimiter "," -Path "C:\ST_$SchoolNumber.csv" | Where-Object {$_.STATUS -match 'ACTV|LVNG'}
@@ -27,11 +40,11 @@ ForEach ($Student In $Students)
 	$GroupMember = $Description + "s " + $Student.'TAG'
 	$StartDate = $Student.'ENTRY'
 	$PrincipalName = $AccountName + "@" + $DomainName
+	$ComplexPassword = ComplexPassword
 	if (($ExistingStudents | Where-Object {$_.sAMAccountName -eq $AccountName}) -eq $null)
 		{
-		$InitialPassword = Invoke-WebRequest -Uri http://www.dinopass.com/password/strong
-		New-ADUser -Name "$AccountName" -DisplayName "$DisplayName" -SamAccountName $AccountName -UserPrincipalName $PrincipalName -GivenName "$FirstName" -Surname "$LastName" -Initials "$SecondNameInitial" -Description "$Description - Start Date $StartDate" -AccountPassword (ConvertTo-SecureString $InitialPassword -AsPlainText -Force) -Enabled $true -Path "$OrganisationalUnit" -ChangePasswordAtLogon $true –PasswordNeverExpires $false -AllowReversiblePasswordEncryption $false
+		New-ADUser -Name "$AccountName" -DisplayName "$DisplayName" -SamAccountName $AccountName -UserPrincipalName $PrincipalName -GivenName "$FirstName" -Surname "$LastName" -Initials "$SecondNameInitial" -Description "$Description - Start Date $StartDate" -AccountPassword (ConvertTo-SecureString $ComplexPassword -AsPlainText -Force) -Enabled $true -Path "$OrganisationalUnit" -ChangePasswordAtLogon $true –PasswordNeverExpires $false -AllowReversiblePasswordEncryption $false
 		Add-ADGroupMember -Identity "$GroupMember" -Members "$AccountName"
-		Write-Host 'UserName: '$AccountName 'FullName: '$DisplayName 'GroupMember: '$GroupMember 'Initial Password: '$InitialPassword
+		Write-Host 'UserName: '$AccountName 'FullName: '$DisplayName 'GroupMember: '$GroupMember 'Initial Password: '$ComplexPassword
 		}
 }
