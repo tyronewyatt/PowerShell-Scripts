fb5288ade49b54e094e23fc15720c066b6f3da6d
tyronewyatt <tyronewyatt@users.noreply.github.com>
2017-06-27 10:50:11 +1000

    Add CSVPath variable

--- ST_Disable-ADUser.ps1
+++ ST_Disable-ADUser.ps1
@@ -2,12 +2,14 @@ Import-Module ActiveDirectory
 
 $SchoolNumber = '8370'
 $StudentsOrganisationalUnit = 'OU=Students,OU=Domain Users,DC=tallangatta-sc,DC=vic,DC=edu,DC=au'
+$CSVPath = '.'
+
 $ExistingStudents = Get-ADUser `
 	-SearchBase $StudentsOrganisationalUnit `
 	-Filter {Enabled -eq $True} `
 	-Properties samAccountName,Description
 
-$Students = Import-Csv -Delimiter "," -Path "C:\ST_$SchoolNumber.csv" | Where-Object {$_.STATUS -match 'LEFT|DEL'}
+$Students = Import-Csv -Delimiter "," -Path "$CSVPath\ST_$SchoolNumber.csv" | Where-Object {$_.STATUS -match 'LEFT|DEL'}
 ForEach ($Student In $Students)
 {
     $AccountName = $Student.'STKEY'
