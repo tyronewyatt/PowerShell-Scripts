b891c85f307b71783a24e3409008ef26725a3e96
tyronewyatt <tyrone.wyatt@gmail.com>
2017-07-06 09:02:53 +1000

    Mail support

--- ST_New-ADUser.ps1
+++ ST_New-ADUser.ps1
@@ -7,6 +7,9 @@ $DomainName = 'tallangatta-sc.vic.edu.au'
 $Description = 'Student'
 $PasswordLength = '7'
 $CSVPath = '\\tscweb02\eduhub$'
+$SmtpServer = 'tscmx01.tallangatta-sc.vic.edu.au'
+$MailTo = 'Netbook Admin <netbookadmin@tallangatta-sc.vic.edu.au>'
+$MailFrom = 'ST_Disable-ADUser <tscdc01@tallangatta-sc.vic.edu.au>'
 
 $ExistingStudents = Get-ADUser `
 	-SearchBase $OrganisationalUnitBase `
@@ -56,6 +59,26 @@ ForEach ($Student In $Students)
 		Add-ADGroupMember `
 			-Identity "$GroupMember" `
 			-Members "$AccountName" `
-		Write-Host 'UserName: '$AccountName 'FullName:' $DisplayName 'GroupMember:' $GroupMember 'Initial Password:' $ComplexPassword
+		Write-Host "UserName: $AccountName FullName: $DisplayName Status: $Status GroupMember: $GroupMember Initial Password: $ComplexPassword"
+		$MailBody += @("`nUserName: $AccountName FullName: $DisplayName Status: $Status GroupMember: $GroupMember Initial Password: $ComplexPassword")
 		}
 }
+
+If ($MailBody -ne $Null)
+	{
+	$NumberAccountsDisabled = ($MailBody).count
+	If (($MailBody).count -eq '1') 
+		{$MailSubject = "Created $NumberAccountsDisabled Account"}
+		Else
+		{$MailSubject = "Created $NumberAccountsDisabled Accounts"}
+	ForEach ($MailBody In $MailBodys)
+		{
+		$MailBody = $MailBody
+		}
+	Send-MailMessage `
+		-To "$MailTo" `
+		-From "$MailFrom" `
+		-Subject "$MailSubject" `
+		-SmtpServer "$SmtpServer" `
+		-Body "$MailBody"
+	}
