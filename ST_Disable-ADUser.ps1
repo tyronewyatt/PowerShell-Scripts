19c27df472657a64c445afd8e65350d66a3a9a3d
tyronewyatt <tyrone.wyatt@gmail.com>
2017-07-06 08:56:16 +1000

    Cleanup

--- ST_Disable-ADUser.ps1
+++ ST_Disable-ADUser.ps1
@@ -4,6 +4,9 @@ $SchoolNumber = '8370'
 $StudentsOrganisationalUnit = 'OU=Students,OU=Domain Users,DC=tallangatta-sc,DC=vic,DC=edu,DC=au'
 $CSVPath = '\\tscweb02\eduhub$'
 $Description = 'Student'
+$SmtpServer = 'tscmx01.tallangatta-sc.vic.edu.au'
+$MailTo = 'Netbook Admin <netbookadmin@tallangatta-sc.vic.edu.au>'
+$MailFrom = 'ST_Disable-ADUser <tscdc01@tallangatta-sc.vic.edu.au>'
 
 $ExistingStudents = Get-ADUser `
 	-SearchBase $StudentsOrganisationalUnit `
@@ -25,15 +28,36 @@ ForEach ($Student In $Students)
 	ElseIf ($DestArrivalDate.length -ne '0')
 		{$Date = $DestArrivalDate}
 		
-	If (($ExistingStudents | Where-Object {$_.sAMAccountName -eq $AccountName}) -ne $null)
+	If (($ExistingStudents | Where-Object {$_.sAMAccountName -eq $AccountName}) -ne $Null)
         {
 		Disable-ADAccount `
 			-Identity $AccountName
-		If($?)			{
+		If ($?)
+			{
 			Set-ADUser `
 				-Identity $AccountName `
 				-Description "$Description - $Status $Date"
 			Write-Host $AccountName 'Disabled. Description' $Description '-' $Status $Date
+			$MailBody += @("`n$AccountName Disabled. Description $Description - $Status $Date")
 			}
 		}
 }
+
+If ($MailBody -ne $Null)
+	{
+	$NumberAccountsDisabled = ($MailBody).count
+	If (($MailBody).count -eq '1') 
+		{$MailSubject = "Disabled $NumberAccountsDisabled Account"}
+		Else
+		{$MailSubject = "Disabled $NumberAccountsDisabled Accounts"}
+	ForEach ($MailBody In $MailBodys)
+		{
+		$MailBody = $MailBody
+		}
+	Send-MailMessage `
+		-To "$MailTo" `
+		-From "$MailFrom" `
+		-Subject "$MailSubject" `
+		-SmtpServer "$SmtpServer" `
+		-Body "$MailBody"
+	}
