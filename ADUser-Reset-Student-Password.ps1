5f6aed1a99b61e2be71ccf301aa49a0c831c8baf
tyronewyatt <tyrone.wyatt@gmail.com>
2017-08-05 09:42:07 +1000

    Replace wrong file with correct

--- ADUser-Reset-Student-Password.ps1
+++ ADUser-Reset-Student-Password.ps1
@@ -3,105 +3,121 @@ Add-Type -AssemblyName System.web
 
 $PasswordLength = '7'
 $RunAsUser = $env:UserName.ToUpper()
-$SmtpServer = 'mail.netspace.net.au'
-$MailTo = 'Admin <tw@tallangattaps.vic.edu.au>'
-$MailFrom = 'ICT Helpdesk <ict.helpdesk@tallangattaps.vic.edu.au>'
+$SmtpServer = 'tscmx01.tallangatta-sc.vic.edu.au'
+$MailTo = 'Netbook Admin <netbookadmin@tallangatta-sc.vic.edu.au>'
+$MailFrom = 'ICT Helpdesk <ict.helpdesk@tallangatta-sc.vic.edu.au>'
 $MailSignature = `
 "ICT Helpdesk
-Tallangatta Primary School
-1 Wonga Grove Tallangatta, 3700, VIC
-t: 02 6071 2590
-e: ict.helpdesk@tallangattaps.vic.edu.au
-w: www.tallangattaps.vic.edu.au"
+Tallangatta Secondary College
+145 Towong Street Tallangatta, 3700, VIC
+t: 02 6071 5000 | f: 02 6071 2445
+e: ict.helpdesk@tallangatta-sc.vic.edu.au
+w: www.tallangatta-sc.vic.edu.au"
 
-$ExistingStudents = Get-ADUser `
-	-SearchBase $OrganisationalUnitBase `
-	-Filter * `
-	-Properties samAccountName
+$UserName = Read-Host -Prompt 'Enter Student Username'
 
-$Students = Import-Csv -Delimiter "," -Path "$CSVPath\ST_$SchoolNumber.csv" | Where-Object {$_.STATUS -match 'FUT|ACTV'}
-ForEach ($Student In $Students)
+Try
+	{
+	$User = Get-ADUser `
+		$UserName `
+		-Properties samAccountName,displayName,enabled
+	} 
+Catch
+	{
+	Write-Host "Username $UserName not found!"
+	Write-Host 'Press any key to exit'
+	[void][System.Console]::ReadKey($True)
+	Exit
+	}
+	
+$AccountName = $User.'samAccountName'.ToUpper()
+$FullName = $User.'displayName'
+$AccountStatus = $User.'enabled'
+
+$CheckUser = "Reset password for $FullName ($AccountName) [y/n]"
+$ConfirmUser = Read-Host "$CheckUser"
+While($ConfirmUser -ne "y")
 {
-	$AccountName = $Student.'STKEY'
-	$LastName = $Student.'SURNAME'
-	$FirstName = $Student.'FIRST_NAME'
-	$SecondName = $Student.'SECOND_NAME'
-	$PreferredName = $Student.'PREF_NAME'
-	If ($Student.'SECOND_NAME'.length -eq '0')
-		{$SecondNameInitial = $null}
-	ElseIF ($Student.'SECOND_NAME'.length -eq '1')
-		{$SecondNameInitial = $Student.'SECOND_NAME'}
-	Else
-		{$SecondNameInitial = $Student.'SECOND_NAME'.Substring(0,1)}
-	$DisplayName = $Student.'FIRST_NAME' + " " + $Student.'SURNAME'
-	If ($Student.'TAG' -match "\*\**")
-		{$TimetableGroup = ($Student.'TAG').substring(2)}
-	Else
-		{$TimetableGroup = $Student.'TAG'}
-	$OrganisationalUnit = "OU=" + $TimetableGroup + "," + $OrganisationalUnitBase
-	$GroupMember = $Description + "s " + $TimetableGroup
-	$StartDate = $Student.'ENTRY'
-	$Status = $Student.'STATUS'
-	$PrincipalName = $AccountName + "@" + $DomainName
-	$ComplexPassword = [System.Web.Security.Membership]::GeneratePassword($PasswordLength,1)
-	If (($ExistingStudents | Where-Object {$_.sAMAccountName -eq $AccountName}) -eq $Null)
-		{
-		New-ADUser `
-			-Name "$AccountName" `
-			-DisplayName "$DisplayName" `
-			-SamAccountName "$AccountName" `
-			-UserPrincipalName "$PrincipalName" `
-			-GivenName "$FirstName" `
-			-Surname "$LastName" `
-			-Initials "$SecondNameInitial" `
-			-Description "$Description" `
-			-AccountPassword (ConvertTo-SecureString $ComplexPassword -AsPlainText -Force) `
-			-Enabled $true `
-			-Path "$OrganisationalUnit" `
-			-ChangePasswordAtLogon $true `
-			–PasswordNeverExpires $false `
-			-AllowReversiblePasswordEncryption $false
-		Add-ADGroupMember `
-			-Identity "$GroupMember" `
-			-Members "$AccountName"
-			If ($?)
-				{
-				Write-Host "UserName: $AccountName FullName: $DisplayName Status: $Status GroupMember: $GroupMember Initial Password: $ComplexPassword"
-				$MailBody += @("`nUserName: $AccountName FullName: $DisplayName Status: $Status GroupMember: $GroupMember Initial Password: $ComplexPassword")
-				}
-		}
+    If ($ConfirmUser -eq 'n') {Exit}
+    ConfirmUser = Read-Host "$CheckUser"
 }
 
-If ($MailBody -ne $Null)
+If ($AccountStatus -eq $False)
+		{
+		Write-Host "User account is disabled!"
+		Write-Host 'Please enable account and try again or contact your Administrator'
+		Write-Host 'Press any key to exit'
+		[void][System.Console]::ReadKey($True)
+		Exit
+		}
+
+$AccountNameLength = $AccountName.Length
+Do { 
+	$AccountNamePasswordDoCount++
+	$AccountNamePasswordVariable = $AccountName.Substring($AccountNamePasswordDoCount-1,3)
+	$AccountNamePasswordArray += ("$AccountNamePasswordVariable|")
+	} while($AccountNamePasswordDoCount -ne $AccountNameLength-2) 
+$FullNameLength = $FullName.Length
+Do { 
+	$FullNamePasswordDoCount++
+	$FullNamePasswordVariable = $FullName.Substring($FullNamePasswordDoCount-1,3)
+	$FullNamePasswordArray += ("$FullNamePasswordVariable|")
+	} while($FullNamePasswordDoCount -ne $FullNameLength-2) 
+Do 	{
+	$ComplexPassword = [System.Web.Security.Membership]::GeneratePassword($PasswordLength,1)
+	}
+Until (
+	$ComplexPassword -match '[A-Z]' -And `
+	$ComplexPassword -match '[0-9]' -And `
+	$ComplexPassword -notmatch "[$AccountNamePasswordArray]|[$FullNamePasswordArray]"
+	)
+
+Set-ADAccountPassword `
+	-Identity $AccountName `
+	-Reset `
+	-NewPassword (ConvertTo-SecureString -AsPlainText $ComplexPassword -Force)
+If ($?)
 	{
-	$NumberAccountsCreated = ($MailBody).count
-	If ($NumberAccountsCreated -eq '1') 
-		{
-		$MailSubject = "Created 1 uaer account"
-		$MailHeading = "The following user account has been created:"
-		}
-	Else 
-		{
-		$MailSubject = "Created $NumberAccountsCreated user accounts"
-		$MailHeading = "The following user accounts have been created:"
-		}
-	ForEach ($MailBody In $MailBodys)
-		{
-		$MailBody = $MailBody
-		}
-		
+	Set-AdUser `
+		-Identity $AccountName `
+		-ChangePasswordAtLogon $true
+	$ComplexPassword | Clip.exe
+	Write-Host "Password: $ComplexPassword"
+	Write-Host 'Password has been copied to clipboard'
+	Write-Host 'Temporary passwords will expire in 10 days if not changed'
+	$MailHeading = "AccountName: $AccountName FullName: $FullName Password: $ComplexPassword"
+	$MailSubject = "Reset password for 1 user account"
+	}
+
+	
+$User = Get-ADUser `
+	$RunAsUser `
+	-Properties Mail,displayName
+$RunAsUserMail = $User.'Mail'
+$RunAsUserFullName = $User.'displayName'
+If ($RunAsUserMail -ne $Null)
+		{$MailCC = "$RunAsUserFullName <$RunAsUserMail>"}
+		Else
+		{$MailCC = 'No Reply <no-reply@tallangattaps.vic.edu.au>'}
+
+	
 $MailBody = `
 "Hello Administrator,
 
 $MailHeading
 $MailBody
+Reset by $RunAsUser.
 
-$MailSignature"
-
-	Send-MailMessage `
-		-To "$MailTo" `
-		-From "$MailFrom" `
-		-Subject "$MailSubject" `
-		-SmtpServer "$SmtpServer" `
-		-Body "$MailBody"
-	}
+$MailSignature"	
+		
+Send-MailMessage `
+	-To "$MailTo" `
+	-Cc "$MailCC" `
+	-From "$MailFrom" `
+	-Subject "$MailSubject" `
+	-SmtpServer "$SmtpServer" `
+	-Body "$MailBody"
+	
+Write-Host 'Press any key to exit'
+[void][System.Console]::ReadKey($True)
+Exit
