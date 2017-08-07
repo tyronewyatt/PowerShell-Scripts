79998df15cdc146a56db6018cd2af842a26797aa
tyronewyatt <tyronewyatt@users.noreply.github.com>
2017-08-07 13:15:25 +1000

    email user their password

--- ADUser-Reset-Student-Password.ps1
+++ ADUser-Reset-Student-Password.ps1
@@ -5,7 +5,9 @@ $PasswordLength = '7'
 $RunAsUser = $env:UserName.ToUpper()
 $SmtpServer = 'tscmx01.tallangatta-sc.vic.edu.au'
 $MailTo = 'Netbook Admin <netbookadmin@tallangatta-sc.vic.edu.au>'
+$MailNoReply = 'No Reply <no-reply@tallangatta-sc.vic.edu.au>'
 $MailFrom = 'ICT Helpdesk <ict.helpdesk@tallangatta-sc.vic.edu.au>'
+$SupportURL = 'https://helpdesk.tallangatta-sc.vic.edu.au'
 $MailSignature = `
 "ICT Helpdesk
 Tallangatta Secondary College
@@ -20,7 +22,7 @@ Try
 	{
 	$User = Get-ADUser `
 		$UserName `
-		-Properties samAccountName,displayName,enabled
+		-Properties samAccountName,givenName,mail,displayName,enabled
 	} 
 Catch
 	{
@@ -31,8 +33,10 @@ Catch
 	}
 	
 $AccountName = $User.'samAccountName'.ToUpper()
+$FirstName = $User.'givenName'
 $FullName = $User.'displayName'
 $AccountStatus = $User.'enabled'
+$mail = $User.'mail'
 
 $CheckUser = "Reset password for $FullName ($AccountName) [y/n]"
 $ConfirmUser = Read-Host "$CheckUser"
@@ -98,8 +102,7 @@ $RunAsUserFullName = $User.'displayName'
 If ($RunAsUserMail -ne $Null)
 		{$MailCC = "$RunAsUserFullName <$RunAsUserMail>"}
 		Else
-		{$MailCC = 'No Reply <no-reply@tallangattaps.vic.edu.au>'}
-
+		{$MailCC = "$MailNoReply"}
 	
 $MailBody = `
 "Hello Administrator,
@@ -117,7 +120,51 @@ Send-MailMessage `
 	-Subject "$MailSubject" `
 	-SmtpServer "$SmtpServer" `
 	-Body "$MailBody"
-	
+
+
+If ($mail -ne $Null)
+		{$MailTo = "$FullName <$mail>"}
+		Else
+		{$MailTo = "$MailNoReply"}
+$MailSubject = "Your password has been reset"
+
+$MailBody = `	
+"Hello $FirstName,
+ 	 
+Your school password has been reset. Please note your temporary school password below:
+ 	 
+School Password: $ComplexPassword
+ 	 
+For security purposes, you must change your password. To change your password, logon to a school computer and follow the prompts.
+
+Important:
+
+When you change your password, you must also update any other PC or device with your school username and password stored on it. Devices may include a notebook, iPad, other tablet, mobile phone and any other PC you use, including those at home.
+
+Your new password must meet the following complexity requirements:
+	1)	Unique password not matching your past eight passwords
+	2)	Be at least seven characters in length
+	3)	Contain characters from three of the following four categories:
+		a)	English uppercase characters (A through Z)
+		b)	English lowercase characters (a through z)
+		c)	Base 10 digits (0 through 9)
+		d)	Non-alphabetic characters (for example, !, $, #, %)
+	4)	Not contain your username or parts of your full name that exceed two consecutive characters
+
+For further assistance:
+	1)	Speak to the school ICT technician and school staff only.
+	2)	Log a request on the school ICT Helpdesk, see: ($SupportURL).
+
+$MailSignature"
+
+Send-MailMessage `
+	-To "$MailTo" `
+	-From "$MailFrom" `
+	-Subject "$MailSubject" `
+	-SmtpServer "$SmtpServer" `
+	-Body "$MailBody"
+
+Write-Host 'Password Reset Successful!'	
 Write-Host 'Press any key to exit'
 [void][System.Console]::ReadKey($True)
 Exit
