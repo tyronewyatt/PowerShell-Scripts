4e47aba143a428d28a0c36c8f90eb2d0b3f3c997
Tyrone Wyatt <tw_tec@tallangatta-sc.vic.edu.au>
2018-08-24 00:10:49 +1000

    cleanup

--- eduMail-Staff-List.ps1
+++ eduMail-Staff-List.ps1
@@ -23,7 +23,7 @@ $Domain = 'edu001'
 
 # Domain Controller
 $DomainControllerFQDN = $SchoolID + "edudc01.education.vic.gov.au"
-Write-Host 'Resolving domain controller...'
+Write-Host 'Getting domain controller'
 Try 
     {$ResolveDnsName = Resolve-DnsName $DomainControllerFQDN -Server 10.10.22.11,10.10.22.12 -ErrorAction Stop}
 Catch 
@@ -32,18 +32,18 @@ Catch
 	Break 
     }
 $DomainControllerIP = ($ResolveDnsName).IPAddress
-Write-Host -ForegroundColor Green "Domain controller set to $DomainControllerFQDN [$DomainControllerIP]."
+Write-Host -ForegroundColor Green "$DomainControllerFQDN [$DomainControllerIP] domain controller found"
 
 # Credentials
 $Username = Read-Host "Enter Username [$Domain]"
 $Password = Read-host 'Enter Password' -AsSecureString
 $Credential = New-Object -TypeName System.Management.Automation.PsCredential -ArgumentList $Domain\$Username,$Password
 
-# SearchBase
-Write-Host "Detecting active directory search base..."
+# School Account
 $SchoolAccountID = 's' + $SchoolID + '01'
 Try 
     {
+	Write-Host 'Getting school details'
     $SchoolAccount = Get-ADUser `
         -Server $DomainControllerIP `
         -Credential $Credential `
@@ -59,12 +59,12 @@ Catch
 $SchoolAccountDN = $SchoolAccount.DistinguishedName
 $SchoolOffice = $SchoolAccount.Office
 $SearchBase = $SchoolAccountDN.Substring($SchoolAccountDN.IndexOf(',OU=')+1)
-Write-Host -ForegroundColor Green "Search base set to $SearchBase."
+Write-Host -ForegroundColor Green "Found $SchoolOffice"
 
-# Get AD Users
-Write-Host 'Getting users for' $SchoolOffice 'from active directory...'
-Try 
+# Get Users
+Try
     {
+	Write-Host 'Getting users'
     $ADUsers = Get-ADUser `
 	    -Server $DomainControllerIP `
         -Credential $Credential `
@@ -90,14 +90,28 @@ Catch
     Write-Host -ForegroundColor Red -BackgroundColor Black $Error[0]
 	Break
     }
-Write-Host -ForegroundColor Green "Found" ($ADUsers).Count "users."
-
+If (($ADUsers).Count -Eq '0') {Write-Warning "No users found"}
+ElseIf (($ADUsers).Count -Eq '1') {Write-Host -ForegroundColor Green "Found 1 user"}
+Else {Write-Host -ForegroundColor Green "Found" ($ADUsers).Count "users"}
 
 # Export CSV
-Write-Host 'Exporting CSV...'
 $Date = (Get-Date).ToString("yyyy-MM-dd")
 $Path = "$SchoolOffice Staff List $Date.csv"
-$ADUsers | Export-csv `
-		-NoTypeInformation `
-		-Path $Path
-If ($?) {Write-Host -ForegroundColor Green "Exported $(Get-Location)\$Path"}
+Try 
+    {
+	If (($ADUsers).Count -Gt '0')
+		{
+		Write-Host 'Exporting staff list'
+		If ((Test-Path $Path)) {Remove-Item $Path}
+		$ADUsers | Export-csv `
+			-NoTypeInformation `
+			-Path $Path `
+			-ErrorAction Stop
+		}
+	}
+Catch 
+    {
+    Write-Host -ForegroundColor Red -BackgroundColor Black $Error[0]
+	Break
+    }
+If ((Test-Path $Path)) {Write-Host -ForegroundColor Green "Exported $(Get-Location)\$Path"}
