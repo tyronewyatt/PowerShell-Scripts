bb16ef20a96400496f9f90b92aec53935ad4e187
tyronewyatt <tyronewyatt@users.noreply.github.com>
2018-11-13 22:13:58 +1100

    First build
    generate multiple passwords from dinopass api

---
+++ dinopass.ps1
@@ -0,0 +1,13 @@
+﻿Param(
+    [String]$Quantity = $(Read-Host 'Password Quantity'),
+	[String]$Export = $(Read-Host 'Export CSV [Yes/NO]')
+	)
+$Passwords = For($Counter=1
+    $Counter -le $Quantity
+    $Counter++){
+        Invoke-WebRequest https://dinopass.com/password/simple
+        }
+
+If ($Export -Eq 'Yes')
+	{$Passwords | Select-Object Content, RawContentLength | Export-CSV -NoTypeInformation -Path .\dinopass.csv}
+$Passwords | Select-Object Content
