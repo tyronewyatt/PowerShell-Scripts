39ac31fa676e7f64db217798c91ba159f91d976b
Tyrone Wyatt <tw_tec@tallangatta-sc.vic.edu.au>
2018-11-14 15:16:00 +1100

    quick and dirty

---
+++ AD-User-Clear-ProxyAddresses.ps1
@@ -0,0 +1,8 @@
+﻿
+$Users = Get-ADuser -SearchBase "OU=Student,OU=Domain Users,DC=curric,DC=domain,DC=wan" -filter {proxyAddresses -Like '*'} -Properties proxyaddresses 
+
+ForEach ($User In $Users)
+    {
+    $Name = $User.'DistinguishedName'
+    Set-ADUser -Identity $Name -Clear ProxyAddresses -PassThru
+    }
