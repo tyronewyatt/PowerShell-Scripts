7d213a98062763df1152c5327580a7ab641fd993
tyronewyatt <tyrone.wyatt@gmail.com>
2018-11-16 14:23:34 +1100

    Build 1.3
    only don't display to screen if exporting.
    export changed to path.

--- dinopass.ps1
+++ dinopass.ps1
@@ -1,25 +1,27 @@
 ﻿<#
 .Synopsis
-    Commandline interface for dinopass.com
+    Gets passwords from dinopass.com website.
 	
 .Description
-    Create multiple simple or strong passwords and display on screen or export to file.
+    The dinopass Cmdlet downloads passwords from the kids passwords generator website dinopas.com. 
+    
+    These passwords can be created singly or in bulk, with simple or strong password complexity and displayed on screen or exported to CSV.
 
 .Notes
     File Name      : dinopass.ps1
     Author         : Tyrone Wyatt (tyrone.wyatt@gmail.com)
     Prerequisite   : PowerShell V3 over Windows 7 and upper
     Copyright      : Tyrone Wyatt 2018
-	Version        : 1.2
+	Version        : 1.3
 	Creation Date  : 16/11/2018
-	Purpose/Change : First stable build with added function module
+	Purpose/Change : First stable build cmdlet with general enhancements
 
 .link
     Repository     : https://github.com/tyronewyatt/PowerShell-Scripts/
 
 .Example
-	# Create 1 simple password
-    dinopass.ps1 -quantity 1
+	# Create a simple password
+    dinopass.ps1
 
 .Example
 	# Create 5 strong passwords
@@ -27,29 +29,27 @@
 
 .Example
 	# Create 30 strong passwords and export to CSV
-    dinopass.ps1 -quantity 30 -complex -export
+    dinopass.ps1 -quantity 30 -complex -path '.\passwords.csv'
  #>
 # Set varibles
 Param(
 	[Int]$Quantity = '1',
     [switch]$Complex,
-    [switch]$Export
+    [string]$Path
 )
 
 # Set default password strength
-If ($Complex -Eq $True) {$Strength = 'strong'}
-Else
-{$Strength = 'simple'}
+If ($Complex -Eq $True) {$Strength = 'strong'} Else {$Strength = 'simple'}
 
 # Set dinopass API URL
 $DinoPassURL = 'https://dinopass.com/password'
 
 # Generate multiple passwords from website
-$Passwords = For($Counter=1
-    $Counter -le $Quantity
-    $Counter++){
-        Invoke-WebRequest $DinoPassURL/$Strength
-    }
+$Passwords = For( ` 
+        $Counter=1
+        $Counter -le $Quantity
+        $Counter++ `
+        ) {Invoke-WebRequest $DinoPassURL/$Strength}
 
 # Change title from Content to Password
 $Passwords = $Passwords | Add-Member -MemberType AliasProperty -Name Password -Value Content -PassThru
@@ -58,8 +58,7 @@ $Passwords = $Passwords | Add-Member -MemberType AliasProperty -Name Password -V
 $Passwords = $Passwords | Add-Member -MemberType AliasProperty -Name PasswordLength -Value RawContentLength -PassThru
 
 # Export to CSV
-If ($Export -Eq $True)
-    {$Passwords | Select-Object Password, PasswordLength | Export-CSV -NoTypeInformation -Path .\dinopass.csv}
+If ($Path) {$Passwords | Select-Object Password, PasswordLength | Export-CSV -NoTypeInformation -Path $Path}
 
 # Write to Screen
-Write-Output $Passwords | Select-Object Password
+If (!$Path) {Write-Output $Passwords | Select-Object Password}
