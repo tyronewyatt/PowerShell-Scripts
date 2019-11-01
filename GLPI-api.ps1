292f4c2c7618128f0111ba31a3d3bafca87c2f0f
tyronewyatt <tyrone.wyatt@gmail.com>
2019-11-01 16:24:51 +1100

    Update GLPI-api.ps1

--- GLPI-api.ps1
+++ GLPI-api.ps1
@@ -5,17 +5,27 @@ $AppToken = 'AzLVtjC1ltRqXpsSBExv3NmxyBSp5Tmjj1NpzjPZ'
 $AppURL = 'https://helpdesk.tallangatta-sc.vic.edu.au/apirest.php'
 
 #initSession
-$SessionToken = Invoke-RestMethod -Method Get "$AppURL/initSession/" -Headers @{"Content-Type"=$ContentType;"Authorization"=$UserToken;"App-Token"=$AppToken}
+$SessionToken = Invoke-RestMethod -Method Get "$AppURL/initSession/" -Headers @{"Content-Type"=$ContentType; "Authorization"=$UserToken; "App-Token"=$AppToken}
 
-#getMyProfiles
-#Invoke-RestMethod -Method Get "$AppURL/getMyProfiles/" -Headers @{"Content-Type"=$ContentType;"Session-Token"=$SessionToken.session_token;"App-Token"=$AppToken}
+#Computers
+$Computers = Invoke-RestMethod -Method Get "$AppURL/Computer/?range=0-9999&expand_dropdowns=true" -Headers @{"Content-Type"=$ContentType; "Session-Token"=$SessionToken.session_token; "App-Token"=$AppToken; "Accept-Range"="990"}
 
+#Users
+$Users = Invoke-RestMethod -Method Get "$AppURL/User/?range=0-9999" -Headers @{"Content-Type"=$ContentType; "Session-Token"=$SessionToken.session_token; "App-Token"=$AppToken;}
 
-$json = Invoke-RestMethod -Method Get "$AppURL/User/" -Headers @{"Content-Type"=$ContentType;"Session-Token"=$SessionToken.session_token;"App-Token"=$AppToken;"Accept-Range"="max"}
-$json | Select-Object name, comment
+ForEach ($Computer In $Computers)
+    {
+    $ComputerName = $Computer.'Name'
+    $ComputerUserid = $Computer.'users_id'
 
-Invoke-RestMethod -Method Get "$AppURL/Computer/?expand_dropdowns=true" -Headers @{"Content-Type"=$ContentType;"Session-Token"=$SessionToken.session_token;"App-Token"=$AppToken;"Accept-Range"="max"}
+    ForEach ($User In $Users)
+    {
+    $UserName = $User.'Name'
+    $UserComment = $User.'Comment'
 
+    If ($UserName -Like $ComputerUserid -And $userComment -Match 'Leaving|Inactive') {$UserName, $UserComment, $ComputerName -join ","} 
+    }
+}
 
 #killSession
-Invoke-RestMethod -Method Get "$AppURL/killSession/" -Headers @{"Content-Type"=$ContentType;"Session-Token"=$SessionToken.session_token;"App-Token"=$AppToken}
+Invoke-RestMethod -Method Get "$AppURL/killSession/" -Headers @{"Content-Type"=$ContentType; "Session-Token"=$SessionToken.session_token; "App-Token"=$AppToken}
