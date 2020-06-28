ce40e0edb94f2268fe50dda88d959c45fdbb05f6
Tyrone Wyatt <tw_tec@tallangatta-sc.vic.edu.au>
2020-06-28 20:34:59 +1000

    Update GLPI-api.ps1

--- GLPI-api.ps1
+++ GLPI-api.ps1
@@ -5,27 +5,35 @@ $AppToken = 'AzLVtjC1ltRqXpsSBExv3NmxyBSp5Tmjj1NpzjPZ'
 $AppURL = 'https://helpdesk.tallangatta-sc.vic.edu.au/apirest.php'
 
 #initSession
-$SessionToken = Invoke-RestMethod -Method Get "$AppURL/initSession/" -Headers @{"Content-Type"=$ContentType; "Authorization"=$UserToken; "App-Token"=$AppToken}
+$SessionToken = Invoke-RestMethod `
+                    -Method Get "$AppURL/initSession/" `
+                    -Headers @{'Content-Type'=$ContentType; 'Authorization'=$UserToken; 'App-Token'=$AppToken}
 
 #Computers
-$Computers = Invoke-RestMethod -Method Get "$AppURL/Computer/?range=0-9999&expand_dropdowns=true" -Headers @{"Content-Type"=$ContentType; "Session-Token"=$SessionToken.session_token; "App-Token"=$AppToken; "Accept-Range"="990"}
+$Computers = Invoke-RestMethod `
+                -Method Get "$AppURL/Computer/?range=0-9999&expand_dropdowns=true" `
+                -Headers @{'Content-Type'=$ContentType; 'Session-Token'=$SessionToken.session_token; 'App-Token'=$AppToken; "Accept-Range"="990"}
 
 #Users
-$Users = Invoke-RestMethod -Method Get "$AppURL/User/?range=0-9999" -Headers @{"Content-Type"=$ContentType; "Session-Token"=$SessionToken.session_token; "App-Token"=$AppToken;}
+$Users = Invoke-RestMethod `
+            -Method Get "$AppURL/User/?range=0-9999" `
+            -Headers @{'Content-Type'=$ContentType; 'Session-Token'=$SessionToken.session_token; 'App-Token'=$AppToken;}
 
 ForEach ($Computer In $Computers)
     {
     $ComputerName = $Computer.'Name'
     $ComputerUserid = $Computer.'users_id'
+    $UserName = $Users | Where-Object {$_.'Name' -Eq $Computer.'users_id'} | Select-Object -ExpandProperty 'Name'
+    $UserComment = $Users | Where-Object {$_.'Name' -Eq $Computer.'users_id'} | Select-Object -ExpandProperty 'Comment'
 
-    ForEach ($User In $Users)
-    {
-    $UserName = $User.'Name'
-    $UserComment = $User.'Comment'
-
-    If ($UserName -Like $ComputerUserid -And $userComment -Match 'Leaving|Inactive') {$UserName, $UserComment, $ComputerName -join ","} 
+    If ($UserName -Like $ComputerUserid -And $userComment -Match 'Leaving') 
+        {
+        New-Object psobject -Property @{'Username'=$UserName; 'User Comment'=$UserComment; 'Computer'=$ComputerName} | 
+        Select-Object 'Username', 'Computer', 'User Comment'
+        }
     }
-}
 
 #killSession
-Invoke-RestMethod -Method Get "$AppURL/killSession/" -Headers @{"Content-Type"=$ContentType; "Session-Token"=$SessionToken.session_token; "App-Token"=$AppToken}
+Invoke-RestMethod `
+    -Method Get "$AppURL/killSession/" `
+    -Headers @{'Content-Type'=$ContentType; 'Session-Token'=$SessionToken.session_token; 'App-Token'=$AppToken}
