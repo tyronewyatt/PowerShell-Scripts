829a3d6fa4014bc35c2cf545ff212f462ff73ce1
tyronewyatt <tyronewyatt@users.noreply.github.com>
2021-03-23 15:04:43 +1100

    Update NLA-Reset.ps1

--- NLA-Reset.ps1
+++ NLA-Reset.ps1
@@ -1,6 +1,9 @@
 $FirewallProfile = netsh advfirewall monitor show currentprofile
-Try {If ($FirewallProfile -match "Public Profile:") 
-        {Restart-Service -name NlaSvc -Force}
+Try {
+    If ($FirewallProfile -match "Public Profile:") 
+        {
+        Restart-Service -name NlaSvc -Force
+        }
 }
 Catch {exit 1}
 Finally {exit 0}
