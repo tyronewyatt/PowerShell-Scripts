42bef67b3b64b18ca6b61a40c774429201061a2f
tyronewyatt <tyronewyatt@users.noreply.github.com>
2021-03-23 15:11:58 +1100

    Create Encode-Decode-Base64.ps1

---
+++ Encode-Decode-Base64.ps1
@@ -0,0 +1,7 @@
+﻿# Encode to Base64
+$String = "SecretMessage"
+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($String))
+
+# Decpde from Base64
+$String = "U2VjcmV0TWVzc2FnZQ=="
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($String))
