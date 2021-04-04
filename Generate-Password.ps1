0d9fd6a6d48fc55b339540e5edb5b44b3de2d03d
tyronewyatt <tyronewyatt@users.noreply.github.com>
2021-04-04 11:39:02 +1000

    Create Generate-Password.ps1

---
+++ Generate-Password.ps1
@@ -0,0 +1,37 @@
+﻿Param(
+	[Int]$Count = '1',
+    [switch]$Strong,
+    [string]$OutFile
+)
+
+[Reflection.Assembly]::LoadWithPartialName("System.Web")
+
+# Set password strength
+If ($Strong) 
+    {
+    $PasswordLength = '12'
+    $PasswordNumOfNonAlphaChas = '1'
+    }
+Else 
+    {
+    $PasswordLength = '6'
+    $PasswordNumOfNonAlphaChas = '0'
+    }
+
+# Generate multiple passwords from website
+$Passwords = For($Counter=1;$Counter -le $Count;$Counter++) 
+        {
+        $ProgressPreference = 'Continue'
+        Write-Progress `
+            -Activity 'Generating' `
+            -Status "$Counter/$Count" `
+            -PercentComplete ($Counter/$Count*100)
+        $ProgressPreference = 'SilentlyContinue'
+        [System.Web.Security.Membership]::GeneratePassword($PasswordLength,$PasswordNumOfNonAlphaChas)
+        }
+
+# Export to CSV or write to screen
+If ($OutFile) 
+    {$Passwords | Export-CSV -NoTypeInformation -Path $OutFile -Confirm}
+Else
+    {Write-Output $Passwords}
