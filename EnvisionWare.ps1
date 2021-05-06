e00621bd26df3e6d88dcf4347ece0a865fc5a5b2
tyronewyatt <tyronewyatt@users.noreply.github.com>
2021-05-06 14:46:03 +1000

    Create EnvisionWare.ps1

---
+++ EnvisionWare.ps1
@@ -0,0 +1,16 @@
+﻿$envisionwarescript = @'
+<!DOCTYPE Settings>
+<Settings>
+    <version>1</version>
+    <type>PC Reservation Client</type>
+    <entry name="Network: Management Service Auto-Discovery Port">0</entry>
+    <entry name="Network: Management Service IP Address/Host Name">40.126.244.184</entry>
+    <entry name="Network: Management Service Port">9432</entry>
+    <collection name="Process Exceptions">
+        <entry name="LPT:One Print Cost Management">Skip When Closing</entry>
+    </collection>
+</Settings>
+'@
+
+
+$envisionwarescript | Out-File "c:\ProgramData\EnvisionWare\PC Reservation\Client Module\config\pcrClient.ewp" -Encoding Ascii
