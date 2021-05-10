6306db6b286ce8ba005e34c9a7f9a041564f1256
tyronewyatt <tyronewyatt@users.noreply.github.com>
2021-05-10 13:43:00 +1000

    Update EnvisionWare.ps1

--- EnvisionWare.ps1
+++ EnvisionWare.ps1
@@ -13,5 +13,5 @@
 '@
 
 
-$envisionwarescript | Out-File "c:\ProgramData\EnvisionWare\PC Reservation\Client Module\config\pcrClient.ewp" -Encoding Ascii
-#($envisionwarescript | Out-String) -replace "`n", "`r`n" | Out-File "c:\ProgramData\EnvisionWare\PC Reservation\Client Module\config\pcrClient.ewp" -Encoding Ascii
+$envisionwarescript | Out-File "c:\ProgramData\pcrClient.ewp" -Encoding Ascii
+($envisionwarescript | Out-String) -replace "`n", "`r`n" | Out-File "c:\ProgramData\pcrClient.ewp" -Encoding Ascii -NoNewline
