1a1f0990eb83a85a0a05d9dab3c4581a62663a3e
Tyrone Wyatt <Tyrone.Wyatt@shellharbour.nsw.gov.au>
2021-07-21 17:14:33 +1000

    Create Get-Uptime.ps1

---
+++ Get-Uptime.ps1
@@ -0,0 +1,6 @@
+﻿$LastBootUpTime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
+$Date = Get-Date
+$Date - $LastBootUpTime |
+Select @{name="Days";expression={$_.Days}}, 
+    @{name="Hours";expression={$_.Hours}}, 
+    @{name="Minutes";expression={$_.Minutes}}
