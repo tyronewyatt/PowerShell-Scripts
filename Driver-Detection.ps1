5c7c4199eb2678332c9dd1e26d704710383bb3c4
Tyrone Wyatt <Tyrone.Wyatt@shellharbour.nsw.gov.au>
2021-07-21 17:14:25 +1000

    Create Driver-Detection.ps1

---
+++ Driver-Detection.ps1
@@ -0,0 +1,13 @@
+﻿$PnPSignedDriver = Get-WmiObject Win32_PnPSignedDriver
+# Application detection state: Installed
+If ($PnPSignedDriver.DeviceName -Eq "Realtek Audio" -And $PnPSignedDriver.DriverVersion -Eq "6.0.9107.1")
+    {
+    Write-Host $True
+    Exit 0
+    }
+# Application detection state: Not installed
+ElseIf ($PnPSignedDriver.DeviceName -Eq "Realtek Audio" -And $PnPSignedDriver.DriverVersion -Eq "6.0.8895.1")
+    {Exit 0}
+# Application detection state: Unknown
+Else 
+    {Exit 1}
