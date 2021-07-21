b8ef6e8d0c85e48eb3ad8e49748980b3feb19fe1
Tyrone Wyatt <Tyrone.Wyatt@shellharbour.nsw.gov.au>
2021-07-21 18:36:39 +1000

    Update Driver-Install.ps1

--- Driver-Install.ps1
+++ Driver-Install.ps1
@@ -1,18 +1,24 @@
 Start-Transcript "$($env:windir)\temp\Realtek-High-Definition-Audio-Driver_88MRG_WIN_6.0.9107.1_A26.log"
 
-$PnPSignedDriver = Get-WmiObject Win32_PnPSignedDriver
-If ($PnPSignedDriver.DeviceName -Eq "Realtek Audio" -And $PnPSignedDriver.DriverVersion -Lt "6.0.9107.1") 
+$DriverVersion = Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.DeviceName -Eq "Realtek Audio"} | Select-Object -ExpandProperty DriverVersion 
+
+# Application Install
+If ($DriverVersion -Lt "6.0.9107.1") 
     {
     Get-ChildItem ".\" -Recurse -Filter "*.inf" | ForEach-Object {& PNPUtil.exe /install /add-driver $_.FullName}
     Stop-Transcript
     Exit 3010
     }
-ElseIf ($PnPSignedDriver.DeviceName -Eq "Realtek Audio" -And $PnPSignedDriver.DriverVersion -Ge "6.0.9107.1") 
+
+# Application Installed
+ElseIf ($DriverVersion -Ge "6.0.9107.1") 
     {
-    Write-Output $True
+    Write-Host $True
     Stop-Transcript
     Exit 0
     }
+
+# Application Unknown
 Else 
     {
     Stop-Transcript | Out-Null
