dae8bd382cbaeaa5df5980513c56aed1618833ab
Tyrone Wyatt <Tyrone.Wyatt@shellharbour.nsw.gov.au>
2021-07-25 01:15:07 +1000

    Update Driver-Detection.ps1

--- Driver-Detection.ps1
+++ Driver-Detection.ps1
@@ -1,17 +1,22 @@
-﻿$PnPSignedDriver = Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.DeviceName -Eq "Realtek Audio"}
+﻿$Manufacturer = 'Dell'
+$Model = 'Latitude 7490'
+$DeviceName = 'Realtek Audio'
+$DriverVersion = '6.0.9107.1'
+
+$PnPSignedDriver = Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.DeviceName -Eq $DeviceName}
 $ComputerSystem = Get-WmiObject Win32_ComputerSystem
 
-If ($ComputerSystem.Manufacturer -Like 'Dell*' -And $ComputerSystem.Model -Eq 'Latitude 7490')
+If ($ComputerSystem.Manufacturer -Like "$Manufacturer*" -And $ComputerSystem.Model -Eq $Model)
     {
     # Application Installed
-    If ($PnPSignedDriver.DriverVersion -Ge '6.0.9107.1')
+    If ($PnPSignedDriver.DriverVersion -Ge $DriverVersion)
         {
         Write-Host $True
         Exit 0
         }
 
     # Application Not installed
-    ElseIf ($PnPSignedDriver.DriverVersion -Lt '6.0.9107.1')
+    ElseIf ($PnPSignedDriver.DriverVersion -Lt $DriverVersion)
         {Exit 0}
     }
 
