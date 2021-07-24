8602a9caa9c609414f1408d5dcac047ecb5dbaca
Tyrone Wyatt <Tyrone.Wyatt@shellharbour.nsw.gov.au>
2021-07-25 00:09:19 +1000

    Update Driver-Install.ps1

--- Driver-Install.ps1
+++ Driver-Install.ps1
@@ -1,21 +1,25 @@
 Start-Transcript "$($env:windir)\temp\Realtek-High-Definition-Audio-Driver_88MRG_WIN_6.0.9107.1_A26.log"
 
-$DriverVersion = Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.DeviceName -Eq "Realtek Audio"} | Select-Object -ExpandProperty DriverVersion 
+$PnPSignedDriver = Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.DeviceName -Eq "Realtek Audio"}
+$ComputerSystem = Get-WmiObject Win32_ComputerSystem
 
-# Application Install
-If ($DriverVersion -Lt "6.0.9107.1") 
+If ($ComputerSystem.Manufacturer -Like 'Dell*' -And $ComputerSystem.Model -Match 'Latitude 7280|Latitude 7290|Latitude 7380|Latitude 7390|Latitude 7480|Latitude 7490')
     {
-    Get-ChildItem ".\" -Recurse -Filter "*.inf" | ForEach-Object {& PNPUtil.exe /install /add-driver $_.FullName}
-    Stop-Transcript
-    Exit 3010
-    }
+    # Application Install
+    If ($PnPSignedDriver.DriverVersion -Lt '6.0.9107.1') 
+        {
+        Get-ChildItem ".\" -Recurse -Filter "*.inf" | ForEach-Object {& PNPUtil.exe /install /add-driver $_.FullName}
+        Stop-Transcript
+        Exit 3010
+        }
 
-# Application Installed
-ElseIf ($DriverVersion -Ge "6.0.9107.1") 
-    {
-    Write-Host $True
-    Stop-Transcript
-    Exit 0
+    # Application Installed
+    ElseIf ($PnPSignedDriver.DriverVersion -Ge '6.0.9107.1') 
+        {
+        Write-Host $True
+        Stop-Transcript
+        Exit 0
+        }
     }
 
 # Application Error
@@ -24,3 +28,4 @@ Else
     Stop-Transcript | Out-Null
     Exit 1
     }
+
