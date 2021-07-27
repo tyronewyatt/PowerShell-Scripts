193b0c590ceb8dfad5b3ed7f48dd85edbd21d56a
tyronewyatt <tyronewyatt@users.noreply.github.com>
2021-07-27 16:19:04 +1000

    Create Set-WLanAutoConnect.ps1

---
+++ Set-WLanAutoConnect.ps1
@@ -0,0 +1,5 @@
+﻿If (((& netsh wlan show interfaces | Select-String State) -replace '.*:\s','') -Eq 'connected')
+    {
+    $SSID = (& netsh wlan show interfaces | Select-String ' SSID') -replace '.*:\s',''
+    & netsh wlan set profileparameter name="$SSID" connectionmode=auto
+    }
