542a7f476888dbd4b276b161974256b92628d388
Tyrone Wyatt <Tyrone.Wyatt@shellharbour.nsw.gov.au>
2021-07-27 23:16:31 +1000

    Update Set-WLanAutoConnect.ps1

--- Set-WLanAutoConnect.ps1
+++ Set-WLanAutoConnect.ps1
@@ -1,4 +1,4 @@
-﻿If (((netsh wlan show interfaces | Select-String State) -replace '.*:\s','') -Eq 'connected')
+﻿If (((netsh wlan show interfaces | Select-String 'State') -replace '.*:\s','') -Eq 'connected')
     {
     (netsh wlan show interfaces | Select-String ' SSID') -replace '.*:\s','' | 
     ForEach-Object {netsh wlan set profileparameter name="$_" connectionmode=auto}
