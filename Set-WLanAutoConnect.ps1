56353511f78547566403e0829ed5c1acfe95295c
Tyrone Wyatt <Tyrone.Wyatt@shellharbour.nsw.gov.au>
2021-07-28 19:58:49 +1000

    Update Set-WLanAutoConnect.ps1

--- Set-WLanAutoConnect.ps1
+++ Set-WLanAutoConnect.ps1
@@ -1,5 +1,8 @@
-﻿$WlanInterfaces = netsh wlan show interfaces
-$WlanState = ($WlanInterfaces | Select-String 'State') -replace '.*:\s',''
-$WlanSSID = ($WlanInterfaces | Select-String ' SSID ') -replace '.*:\s',''
-If ($WlanState -Eq 'connected')
-    {netsh wlan set profileparameter name="$WlanSSID" connectionmode=auto}
+﻿If ((Get-Service -Name 'WlanSvc').Status -Eq 'Running')
+    {
+    $WlanInterfaces = netsh wlan show interfaces
+    $WlanState = ($WlanInterfaces | Select-String 'State') -replace '.*:\s',''
+    $WlanSSID = ($WlanInterfaces | Select-String ' SSID ') -replace '.*:\s',''
+    If ($WlanState -Eq 'connected')
+        {netsh wlan set profileparameter name="$WlanSSID" connectionmode=auto}
+    }
