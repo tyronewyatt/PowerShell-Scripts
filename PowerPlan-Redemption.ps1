327dc4beb255fd1c5a76e64816ea711c90bd2375
tyronewyatt <tyronewyatt@users.noreply.github.com>
2022-05-13 16:30:14 +1000

    Update PowerPlan-Redemption.ps1

--- PowerPlan-Redemption.ps1
+++ PowerPlan-Redemption.ps1
@@ -1,2 +1,16 @@
-﻿# Balanced Power Scheme Personality
-& PowerCfg.exe /s 381b4222-f694-41f0-9685-ff5bb260df2e
+﻿# Power Scheme Personality
+$SchemePersonality = @(
+    [pscustomobject]@{Name='Power Saver';      ID='0'}
+    [pscustomobject]@{Name='High Performance'; ID='1'}
+    [pscustomobject]@{Name='Balanced';         ID='2'}
+)
+
+# Customized Peak (ConfigMgr) Power Scheme
+$PowerScheme = 'db310065-829b-4671-9647-2261c00e86ef'
+
+# Balanced Power Scheme Personality in ConfigMgr Plan
+If ($(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 | Select-Object -ExpandProperty ACSettingIndex) -Eq $SchemePersonality[1].ID)
+    {Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 -Name ACSettingIndex -Value $SchemePersonality[2].ID -Type DWord}
+
+If ($(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 | Select-Object -ExpandProperty DCSettingIndex) -Eq $SchemePersonality[1].ID)
+    {Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 -Name DCSettingIndex -Value $SchemePersonality[2].ID -Type DWord}
