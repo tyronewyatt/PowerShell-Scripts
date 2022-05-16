618df1e5e66f114046301f7e68875f866e991bd5
tyronewyatt <tyronewyatt@users.noreply.github.com>
2022-05-16 11:08:25 +1000

    Update PowerPlan-Detection.ps1

--- PowerPlan-Detection.ps1
+++ PowerPlan-Detection.ps1
@@ -1,15 +1,18 @@
-﻿# Power Scheme Personality
-$SchemePersonality = @(
-    [pscustomobject]@{Name='Power Saver';      ID='0'}
-    [pscustomobject]@{Name='High Performance'; ID='1'}
-    [pscustomobject]@{Name='Balanced';         ID='2'}
+﻿# Customized Peak (ConfigMgr) Power Scheme GUID
+$PowerSchemeGUID = 'db310065-829b-4671-9647-2261c00e86ef'
+
+# Scheme Personality GUID
+$PowerSchemePersonalityGUID = '245d8541-3943-4422-b025-13a784f679b7'
+
+# Power Scheme Personality
+$PowerSchemePersonality = @(
+    [pscustomobject]@{ID='0';Name='Power Saver'}
+    [pscustomobject]@{ID='1';Name='High Performance'}
+    [pscustomobject]@{ID='2';Name='Balanced'}
 )
 
-# Customized Peak (ConfigMgr) Power Scheme
-$PowerScheme = 'db310065-829b-4671-9647-2261c00e86ef'
-
-If ($(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 | Select-Object -ExpandProperty ACSettingIndex) -Eq $SchemePersonality[2].ID `
--And $(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 | Select-Object -ExpandProperty DCSettingIndex) -Eq $SchemePersonality[2].ID)
+If ($(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerSchemeGUID\$PowerSchemePersonalityGUID | Select-Object -ExpandProperty ACSettingIndex) -Eq $PowerSchemePersonality[2].ID `
+-And $(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerSchemeGUID\$PowerSchemePersonalityGUID | Select-Object -ExpandProperty DCSettingIndex) -Eq $PowerSchemePersonality[2].ID)
     {$True} 
 Else
     {$False}
