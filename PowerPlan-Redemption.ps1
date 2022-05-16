72577a7ae19ff08072c22e1d657f285e38737360
tyronewyatt <tyronewyatt@users.noreply.github.com>
2022-05-16 11:08:29 +1000

    Update PowerPlan-Redemption.ps1

--- PowerPlan-Redemption.ps1
+++ PowerPlan-Redemption.ps1
@@ -1,16 +1,19 @@
-﻿# Power Scheme Personality
-$SchemePersonality = @(
-    [pscustomobject]@{Name='Power Saver';      ID='0'}
-    [pscustomobject]@{Name='High Performance'; ID='1'}
-    [pscustomobject]@{Name='Balanced';         ID='2'}
+﻿# Customized Peak (ConfigMgr) Power Scheme GUID
+$PowerSchemeGUID = 'db310065-829b-4671-9647-2261c00e86ef'
+
+# Scheme Personality GUID
+$PowerSchemePersonalityGUID = '245d8541-3943-4422-b025-13a784f679b7'
+
+# Power Scheme Personality
+$PowerSchemePersonality = @(
+    [pscustomobject]@{ID='0';Name='Power Saver'}
+    [pscustomobject]@{ID='1';Name='High Performance'}
+    [pscustomobject]@{ID='2';Name='Balanced'}
 )
 
-# Customized Peak (ConfigMgr) Power Scheme
-$PowerScheme = 'db310065-829b-4671-9647-2261c00e86ef'
-
 # Balanced Power Scheme Personality in ConfigMgr Plan
-If ($(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 | Select-Object -ExpandProperty ACSettingIndex) -Eq $SchemePersonality[1].ID)
-    {Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 -Name ACSettingIndex -Value $SchemePersonality[2].ID -Type DWord}
+If ($(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerSchemeGUID\$PowerSchemePersonalityGUID | Select-Object -ExpandProperty ACSettingIndex) -Eq $PowerSchemePersonality[1].ID)
+    {Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerSchemeGUID\$PowerSchemePersonalityGUID -Name ACSettingIndex -Value $PowerSchemePersonality[2].ID -Type DWord}
 
-If ($(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 | Select-Object -ExpandProperty DCSettingIndex) -Eq $SchemePersonality[1].ID)
-    {Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerScheme\245d8541-3943-4422-b025-13a784f679b7 -Name DCSettingIndex -Value $SchemePersonality[2].ID -Type DWord}
+If ($(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerSchemeGUID\$PowerSchemePersonalityGUID | Select-Object -ExpandProperty DCSettingIndex) -Eq $PowerSchemePersonality[1].ID)
+    {Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$PowerSchemeGUID\$PowerSchemePersonalityGUID -Name DCSettingIndex -Value $PowerSchemePersonality[2].ID -Type DWord}
