8b9e462394b23678736635cb6b0ff679935603df
tyronewyatt <tyronewyatt@users.noreply.github.com>
2022-06-17 16:08:00 +1000

    Create Set-OSDComputerName.ps1

---
+++ Set-OSDComputerName.ps1
@@ -0,0 +1,20 @@
+﻿$SMSTSMachineName = (New-Object -COMObject Microsoft.SMS.TSEnvironment).Value('_SMSTSMachinename')
+If ($SMSTSMachineName -Like 'MININT-*')
+    {
+    # Get Wmi Objects
+    $SerialNumber = (Get-WmiObject Win32_BIOS).SerialNumber
+
+    # Normalize Serial Number
+    $SerialNumber = $SerialNumber -Replace '[-._ ]',''
+
+    # Set Serial Number Length
+    $SerialNumber = $SerialNumber.SubString(0,15)
+
+    # Set ComputerName Variable
+    $OSDComputerName = $SerialNumber
+
+    # Set Computer Name in Operating System Deployment
+    Write-Output $OSDComputerName
+    }
+Else
+    {Write-Output $SMSTSMachineName}
