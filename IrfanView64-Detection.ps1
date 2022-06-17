0c84e2549f490f404479070d98760d289718558d
tyronewyatt <tyronewyatt@users.noreply.github.com>
2022-06-17 16:08:02 +1000

    Create IrfanView64-Detection.ps1

---
+++ IrfanView64-Detection.ps1
@@ -0,0 +1,18 @@
+﻿# Continue Silently on Error
+$ErrorActionPreference = 'SilentlyContinue'
+
+# Wait for Uninstall 
+Wait-Process -Name 'iv_uninstall'
+
+If ($(Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\IrfanView64 | Select-Object -ExpandProperty DisplayVersion) -eq '4.60')
+    {
+    # Application Installed
+    Write-Host $True
+    Exit 0
+    }
+
+    # Application Not installed
+Else 
+    {
+    Exit 0
+    }
