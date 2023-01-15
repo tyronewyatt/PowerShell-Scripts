9dc84e5d0262819e1f0669ff4a2f61ddc383595c
tyronewyatt <tyrone.wyatt@gmail.com>
2023-01-16 09:34:07 +1100

    Update Set-Office365-UpdateChannel.ps1

--- Set-Office365-UpdateChannel.ps1
+++ Set-Office365-UpdateChannel.ps1
@@ -1,13 +1,15 @@
-﻿# Update channel below to Current, Monthly Enterprise or Semi-Annual Enterprise.
-$Channel = 'Monthly Enterprise'
+﻿# Update Channels
+$UpdateChannels = @()
+$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60';Channel='Current'}
+$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/64256afe-f5d9-4f86-8936-8840a6a4f5be';Channel='Current (Preview)'}
+$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/55336b82-a18d-4dd6-b5f6-9e5095c314a6';Channel='Monthly Enterprise'}
+$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114';Channel='Semi-Annual Enterprise'}
+$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/b8f9b850-328d-4355-9145-c59439a0c4cf';Channel='Semi-Annual Enterprise (Preview)'}
+$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/5440fd1f-7ecb-4221-8110-145efaa6372f';Channel='Beta'}
 
-Write-Host Updating Office 365 Channel to $Channel`.
+# Select New Update Channel
+$UpdateChannel = $UpdateChannels | Where-Object Channel -Eq 'Monthly Enterprise' 
 
-$UpdateChannel = @()
-$UpdateChannel += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60';Channel='Current'}
-$UpdateChannel += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/55336b82-a18d-4dd6-b5f6-9e5095c314a6';Channel='Monthly Enterprise'}
-$UpdateChannel += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114';Channel='Semi-Annual Enterprise'}
-$UpdateChannel = $UpdateChannel | Where-Object Channel -Eq $Channel
-
+# Set New Update Channel
 If ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration' | Select-Object -ExpandProperty CDNBaseUrl) -ne $UpdateChannel.CDNBaseUrl) 
     {New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration' -Name CDNBaseUrl -Value $UpdateChannel.CDNBaseUrl -PropertyType String -Force -ea SilentlyContinue}
