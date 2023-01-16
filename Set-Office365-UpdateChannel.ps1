655fc47dbe2acf33c5e06eed09a465671908cd08
tyronewyatt <tyrone.wyatt@gmail.com>
2023-01-16 14:04:57 +1100

    Update Set-Office365-UpdateChannel.ps1

--- Set-Office365-UpdateChannel.ps1
+++ Set-Office365-UpdateChannel.ps1
@@ -1,15 +1,33 @@
 ﻿# Update Channels
 $UpdateChannels = @()
-$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60';Channel='Current'}
-$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/64256afe-f5d9-4f86-8936-8840a6a4f5be';Channel='Current (Preview)'}
-$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/55336b82-a18d-4dd6-b5f6-9e5095c314a6';Channel='Monthly Enterprise'}
-$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114';Channel='Semi-Annual Enterprise'}
-$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/b8f9b850-328d-4355-9145-c59439a0c4cf';Channel='Semi-Annual Enterprise (Preview)'}
-$UpdateChannels += [PSCustomObject] @{CDNBaseUrl='http://officecdn.microsoft.com/pr/5440fd1f-7ecb-4221-8110-145efaa6372f';Channel='Beta'}
+$UpdateChannels += [PSCustomObject] @{
+                                        Channel='Current';
+                                        Value='Current';
+                                    }
+$UpdateChannels += [PSCustomObject] @{
+                                        Channel='Current (Preview)';
+                                        Value='FirstReleaseCurrent';
+                                    }
+$UpdateChannels += [PSCustomObject] @{
+                                        Channel='Monthly Enterprise';
+                                        Value='MonthlyEnterprise';
+                                    }
+$UpdateChannels += [PSCustomObject] @{
+                                        Channel='Semi-Annual Enterprise';
+                                        Value='Deferred';
+                                    }
+$UpdateChannels += [PSCustomObject] @{
+                                        Channel='Semi-Annual Enterprise (Preview)';
+                                        Value='FirstReleaseDeferred';
+                                    }
+$UpdateChannels += [PSCustomObject] @{
+                                        Channel='Beta';
+                                        Value='InsiderFast';
+                                    }
 
 # Select New Update Channel
 $UpdateChannel = $UpdateChannels | Where-Object Channel -Eq 'Monthly Enterprise' 
 
 # Set New Update Channel
-If ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration' | Select-Object -ExpandProperty CDNBaseUrl) -ne $UpdateChannel.CDNBaseUrl) 
-    {New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration' -Name CDNBaseUrl -Value $UpdateChannel.CDNBaseUrl -PropertyType String -Force -ea SilentlyContinue}
+If (-Not(Test-Path -Path 'HKLM:\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate') -eq $true) {New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate' -Force}
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate' -Name updatebranch -Value $UpdateChannel.Value -PropertyType String -Force -ea SilentlyContinue
