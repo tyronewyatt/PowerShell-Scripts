17754b25ee7fc0266ce829ce54321309eab06fff
tyronewyatt <tyrone.wyatt@gmail.com>
2023-02-05 13:26:41 +1100

    Update Set-Office365-UpdateChannel.ps1

--- Set-Office365-UpdateChannel.ps1
+++ Set-Office365-UpdateChannel.ps1
@@ -1,33 +1,33 @@
 ﻿# Update Channels
 $UpdateChannels = @()
 $UpdateChannels += [PSCustomObject] @{
-                                        Channel='Current';
-                                        Value='Current';
+                                        DisplayName = 'Current';
+                                        KeyValue = 'Current';
                                     }
 $UpdateChannels += [PSCustomObject] @{
-                                        Channel='Current (Preview)';
-                                        Value='FirstReleaseCurrent';
+                                        DisplayName = 'Current (Preview)';
+                                        KeyValue = 'FirstReleaseCurrent';
                                     }
 $UpdateChannels += [PSCustomObject] @{
-                                        Channel='Monthly Enterprise';
-                                        Value='MonthlyEnterprise';
+                                        DisplayName = 'Monthly Enterprise';
+                                        KeyValue = 'MonthlyEnterprise';
                                     }
 $UpdateChannels += [PSCustomObject] @{
-                                        Channel='Semi-Annual Enterprise';
-                                        Value='Deferred';
+                                        DisplayName = 'Semi-Annual Enterprise';
+                                        KeyValue = 'Deferred';
                                     }
 $UpdateChannels += [PSCustomObject] @{
-                                        Channel='Semi-Annual Enterprise (Preview)';
-                                        Value='FirstReleaseDeferred';
+                                        DisplayName = 'Semi-Annual Enterprise (Preview)';
+                                        KeyValue  ='FirstReleaseDeferred';
                                     }
 $UpdateChannels += [PSCustomObject] @{
-                                        Channel='Beta';
-                                        Value='InsiderFast';
+                                        DisplayName = 'Beta';
+                                        KeyValue = 'InsiderFast';
                                     }
 
 # Select New Update Channel
-$UpdateChannel = $UpdateChannels | Where-Object Channel -Eq 'Monthly Enterprise' 
+$UpdateChannel = $UpdateChannels | Where-Object DisplayName -Eq 'Monthly Enterprise' 
 
 # Set New Update Channel
 If (-Not(Test-Path -Path 'HKLM:\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate') -eq $true) {New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate' -Force}
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate' -Name updatebranch -Value $UpdateChannel.Value -PropertyType String -Force -ea SilentlyContinue
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate' -Name updatebranch -Value $UpdateChannel.KeyValue -PropertyType String -Force -ea SilentlyContinue
